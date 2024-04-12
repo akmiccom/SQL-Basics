376b2fbe974c277b0966b80024bb15f00b05195a
akmiccom <akmiccom@gmail.com>
2024-04-12 09:25:36 +0900

    first commit

---
+++ -- SQLite.sql
@@ -0,0 +1,18 @@
+-- SQLite
+
+-- 作成
+CREATE TABLE member
+DELETE TABLE member
+
+-- 取得
+SELECT * FROM shoppinglist
+
+-- 追加
+INSERT INTO shoppinglist VALUES(8, '牛肉', 500, '2024-01-01')
+
+-- 更新
+UPDATE shoppinglist
+
+-- 削除
+DELETE FROM shoppinglist
+WHERE id = 8;
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # SQL-Basics
 SQLについて基本的な内容をまとめる
+
+## vscode で sqlite を使う
+- SQLite: Open Databese
+- db の選択
+- .sql に命令文を書き、右クリックで実行
+- Run Query と Select Run Query がある
+ 
+## CREATE
+
+## SELECT
+
+```sql
+SELECT
+```
+
+## INSERT
+
+## DELETE
+
+## UPDATE
